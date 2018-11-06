00107ef3b6fce2dd627db5924c1767df0803a4bf
ablodge <ablodge@users.noreply.github.com>
2018-11-06 16:05:01 -0500

    add tutorial.html

--- build.bat
+++ build.bat
@@ -1,4 +1,5 @@
 C:\Users\austi\Anaconda3\python.exe build-html.py Sentence demo_v1.html data/aligned_amrs.txt data/amr_sentences.txt 0 False %*
 C:\Users\austi\Anaconda3\python.exe build-html.py CCG demo_v2.html data/aligned_amrs.txt data/ccg.txt 1 True %*
 C:\Users\austi\Anaconda3\python.exe build-html.py CCG demo_v2.html data/aligned_amrs.txt data/ccg.txt 1 False %*
+C:\Users\austi\Anaconda3\python.exe build-html.py CCG tutorial.html tutorial/amrs.txt tutorial/ccg.txt 1 False %*
 C:\Users\austi\Anaconda3\python.exe build-js.py %*
